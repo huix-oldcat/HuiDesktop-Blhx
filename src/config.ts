cf23af75c3458e08f7b7928f763c581447f42c24
老猫不发威你当我病危 <oldcat@huix.cc>
2020-11-05 15:44:43 +0800

    使用standard格式格式化代码

--- src/config.ts
+++ src/config.ts
@@ -1,65 +1,67 @@
-const SETTING_VERSION = 2;
+import huiDesktop from './huiDesktop'
+
+const SETTING_VERSION = 2
 
 export enum MouseKeyFunction {
-    void,
-    touch,
-    switchDance,
-    walk
+  void,
+  touch,
+  switchDance,
+  walk
 };
 
 export interface ModelSettings {
-    name: string;
-    location: string;
-    width: number;
-    height: number;
-    x0: number;
-    y0: number;
+  name: string
+  location: string
+  width: number
+  height: number
+  x0: number
+  y0: number
 };
 
 export class UserSettings {
-    posX = 0;
-    posY = 0;
-    left = MouseKeyFunction.void;
-    right = MouseKeyFunction.void;
-    leftDrag = false;
-    rightDrag = false;
-    topMost = false;
-    showInTaskbar = true;
-    opacity = 1;
-    zoom = 1;
-    reverse = false;
-    walkRandom = 0;
-    free = false;
-    version = SETTING_VERSION;
-    clickTransparent = false;
-    constructor(modelSettings: ModelSettings) {
-        this.posY = huiDesktop.WorkingArea.Top + huiDesktop.WorkingArea.Height - modelSettings.height + modelSettings.y0;
-    }
+  posX = 0
+  posY = 0
+  left = MouseKeyFunction.void
+  right = MouseKeyFunction.void
+  leftDrag = false
+  rightDrag = false
+  topMost = false
+  showInTaskbar = true
+  opacity = 1
+  zoom = 1
+  reverse = false
+  walkRandom = 0
+  free = false
+  version = SETTING_VERSION
+  clickTransparent = false
+  constructor (modelSettings: ModelSettings) {
+    this.posY = huiDesktop.WorkingArea.Top + huiDesktop.WorkingArea.Height - modelSettings.height + modelSettings.y0
+  }
 };
 
-function correctSettings(userSettings: UserSettings, modelSettings: ModelSettings) {
-    if (userSettings.version !== SETTING_VERSION) userSettings = new UserSettings(modelSettings);
-    if (userSettings.free == false) userSettings.posY = huiDesktop.WorkingArea.Top + huiDesktop.WorkingArea.Height - modelSettings.height * userSettings.zoom + modelSettings.y0 * userSettings.zoom;
-    if (userSettings.zoom <= 0) userSettings.zoom = 1;
-    if (userSettings.opacity > 1 || userSettings.opacity < 0) userSettings.opacity = 1;
+function correctSettings (userSettings: UserSettings, modelSettings: ModelSettings): void {
+  if (userSettings.version !== SETTING_VERSION) userSettings = new UserSettings(modelSettings)
+  if (!userSettings.free) userSettings.posY = huiDesktop.WorkingArea.Top + huiDesktop.WorkingArea.Height - modelSettings.height * userSettings.zoom + modelSettings.y0 * userSettings.zoom
+  if (userSettings.zoom <= 0) userSettings.zoom = 1
+  if (userSettings.opacity > 1 || userSettings.opacity < 0) userSettings.opacity = 1
 }
 
-export function saveUserSettings(modelName: string, userSettings: UserSettings) {
-    userSettings.posX = huiDesktop.Window.Left;
-    userSettings.posY = huiDesktop.Window.Top;
-    localStorage.setItem('cc.huix.blhx.' + modelName, JSON.stringify(userSettings));
+export function saveUserSettings (modelName: string, userSettings: UserSettings): void {
+  userSettings.posX = huiDesktop.Window.Left
+  userSettings.posY = huiDesktop.Window.Top
+  localStorage.setItem('cc.huix.blhx.' + modelName, JSON.stringify(userSettings))
 }
 
-export function readUserSettings(modelSettings: ModelSettings) {
-    let item = localStorage.getItem('cc.huix.blhx.' + modelSettings.name);
-    let settings: UserSettings;
-    if (item == null) {
-        settings = new UserSettings(modelSettings);
-        saveUserSettings(modelSettings.name, settings);
-    } else {
-        settings = <UserSettings>JSON.parse(item);
-        correctSettings(settings, modelSettings);
-    }
-    (<any>window).userSettings = settings;
-    return settings;
+export function readUserSettings (modelSettings: ModelSettings): UserSettings {
+  const item = localStorage.getItem('cc.huix.blhx.' + modelSettings.name)
+  let settings: UserSettings
+  if (item == null) {
+    settings = new UserSettings(modelSettings)
+    saveUserSettings(modelSettings.name, settings)
+  } else {
+    settings = JSON.parse(item) as UserSettings
+    correctSettings(settings, modelSettings)
+  }
+  // (<any>window).userSettings = settings
+  return settings
 }
--- src/drag.ts
+++ src/drag.ts
@@ -1,55 +1,61 @@
-import share, { AnimationStatus, changeToIdle, flipCharacter } from './share';
-import { ModelSettings, UserSettings, saveUserSettings } from './config';
+import { share, AnimationStatus } from './share'
+import { ModelSettings, UserSettings, saveUserSettings } from './config'
 
-import TWEEN, { Tween } from '@tweenjs/tween.js';
-import { WalkManager } from './walk';
+import huiDesktop, { BasicWindow } from './huiDesktop'
+import PIXI from 'pixi.js'
+import TWEEN, { Tween } from '@tweenjs/tween.js'
+import { WalkManager } from './walk'
 
 export class DragManager {
-    walkMan: WalkManager;
-    modelSettings: ModelSettings;
-    userSettings: UserSettings;
-    animation: PIXI.spine.Spine;
-    tween?: Tween<Record<string, unknown>>;
+  walkMan: WalkManager
+  modelSettings: ModelSettings
+  userSettings: UserSettings
+  animation: PIXI.spine.Spine
+  tween?: Tween<BasicWindow>
 
-    constructor(modelSettings: ModelSettings, userSettings: UserSettings, walkman: WalkManager) {
-        if (share.animation == undefined) throw new Error();
-        this.modelSettings = modelSettings;
-        this.userSettings = userSettings;
-        this.walkMan = walkman;
-        this.animation = share.animation;
-        (<any>window).huiDesktop_DragMove_OnMouseLeftDown = () => this.dragDown();
-        (<any>window).huiDesktop_DragMove_OnMouseRightDown = () => this.dragDown();
-        (<any>window).huiDesktop_DragMove_OnMouseLeftUp = () => this.dragUp();
-        (<any>window).huiDesktop_DragMove_OnMouseRightUp = () => this.dragUp();
-    }
+  constructor (modelSettings: ModelSettings, userSettings: UserSettings, walkman: WalkManager) {
+    if (share.animation === undefined) throw new Error()
+    this.modelSettings = modelSettings
+    this.userSettings = userSettings
+    this.walkMan = walkman
+    this.animation = share.animation
+  }
 
-    dragDown() {
-        if (share.animationStatus == AnimationStatus.flying && this.tween) this.tween.stop();
-        if (share.animationStatus == AnimationStatus.walking) this.walkMan.stopWalking();
-        if (share.animation == undefined) throw new Error();
-        share.animation.state.setAnimation(0, 'tuozhuai2', true);
-        share.animationStatus = AnimationStatus.dragging;
+  public registerToGlobal (): void {
+    const window_ = window as Record<string, any>
+    window_.huiDesktop_DragMove_OnMouseRightDown = () => this.dragDown()
+    window_.huiDesktop_DragMove_OnMouseLeftDown = () => this.dragDown()
+    window_.huiDesktop_DragMove_OnMouseLeftUp = () => this.dragUp()
+    window_.huiDesktop_DragMove_OnMouseRightUp = () => this.dragUp()
+  }
+
+  dragDown (): void {
+    if (share.animationStatus === AnimationStatus.flying) this.tween?.stop()
+    if (share.animationStatus === AnimationStatus.walking) this.walkMan.stopWalking()
+    if (share.animation === undefined) throw new Error()
+    share.animation.state.setAnimation(0, 'tuozhuai2', true)
+    share.animationStatus = AnimationStatus.dragging
+  }
+
+  dragUp (): void {
+    if (share.animation === undefined) throw new Error()
+    if (this.userSettings.free) {
+      share.animationStatus = AnimationStatus.idle
+      share.animation.state.setAnimation(0, share.dancing ? 'dance' : 'stand2', true)
+    } else {
+      share.animationStatus = AnimationStatus.flying
+      share.animation.state.setAnimation(0, share.dancing ? 'dance' : 'stand2', true)
+      this.tween = new TWEEN.Tween(huiDesktop.Window)
+      const target = huiDesktop.WorkingArea.Top + huiDesktop.WorkingArea.Height - this.modelSettings.height * this.userSettings.zoom + this.modelSettings.y0 * this.userSettings.zoom
+      this.tween.to({ Top: target }, 0.666 * Math.abs(target - huiDesktop.Window.Top))
+        .onComplete(() => {
+          if (share.animation === undefined) throw new Error()
+          share.animationStatus = AnimationStatus.idle
+          if (share.dancing) share.animation.state.setAnimation(0, share.dancing ? 'dance' : 'stand2', true)
+          saveUserSettings(this.modelSettings.name, this.userSettings)
+        })
+        .start()
     }
-    
-    dragUp() {
-        if (share.animation == undefined) throw new Error();
-        if (this.userSettings.free) {
-            share.animationStatus = AnimationStatus.idle;
-            share.animation.state.setAnimation(0, share.dancing ? 'dance' : 'stand2', true);
-        } else {
-            share.animationStatus = AnimationStatus.flying;
-            share.animation.state.setAnimation(0, share.dancing ? 'dance' : 'stand2', true);
-            this.tween = new TWEEN.Tween(<Record<string, unknown>><unknown>huiDesktop.Window);
-            let target = huiDesktop.WorkingArea.Top + huiDesktop.WorkingArea.Height - this.modelSettings.height * this.userSettings.zoom + this.modelSettings.y0 * this.userSettings.zoom;
-            this.tween.to({ Top: target }, 0.666 * Math.abs(target - huiDesktop.Window.Top))
-                .onComplete(() => {
-                    if (share.animation == undefined) throw new Error();
-                    share.animationStatus = AnimationStatus.idle;
-                    if (share.dancing) share.animation.state.setAnimation(0, share.dancing ? 'dance' : 'stand2', true);
-                    saveUserSettings(this.modelSettings.name, this.userSettings);
-                })
-                .start();
-        }
-        saveUserSettings(this.modelSettings.name, this.userSettings);
-    }
+    saveUserSettings(this.modelSettings.name, this.userSettings)
+  }
 }
